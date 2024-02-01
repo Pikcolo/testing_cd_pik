3f3fbe9365969e56073522610224f6a637f3a838
PIK <153786025+Pikcolo@users.noreply.github.com>
2024-02-01 22:11:12 +0700

    Delete funny_stack directory

--- funny_stack/funny_stack.py
+++
@@ -1,6 +0,0 @@
-def funnyString(s):
-    a = s[::-1]
-    for i in range(len(s)-1):
-        if abs(ord(s[i]) - ord(s[i+1])) != abs(ord(a[i]) - ord(a[i+1])):
-            return 'Not Funny'
-    return 'Funny'
